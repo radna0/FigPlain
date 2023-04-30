099ac932ada9ad16aa43d146beae16605f497237
Bach HG <87762857+radna0@users.noreply.github.com>
2023-04-30 14:00:45 +0700

    Commit

--- pages/[id].tsx
+++ pages/[id].tsx
@@ -8,7 +8,7 @@ function Search() {
   const { query } = useRouter()
   return (
     <div className=" bg-slate-900 text-white  h-screen text-center">
-      <h1 className="pt-10 pb-4 font-bold text-3xl">Figplain</h1>
+      <h1 className="pt-10 pb-4 font-bold text-3xl">Ixplain</h1>
       <Form />
       <Description query={query.id as string} />
     </div>
--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 function index() {
   return (
     <div className=" bg-slate-900 text-white  h-screen text-center">
-      <h1 className=" pt-10 pb-4 font-bold text-3xl">Figplain</h1>
+      <h1 className=" pt-10 pb-4 font-bold text-3xl">Ixplain</h1>
       <Form />
     </div>
   )
